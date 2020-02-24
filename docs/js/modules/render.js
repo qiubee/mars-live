1f82b7db5d27b0687ff01689ff99e80781c55a49
qiubee <qiubees@hotmail.com>
2020-02-24 14:44:25 +0100

    update function export, add node&text-add function

--- docs/js/modules/render.js
+++ docs/js/modules/render.js
@@ -1,22 +1,32 @@
-function createWeatherCard(weatherData) {
-    const article = document.querySelector("main > article");
-    weatherData.map(function (item) {
+export function createWeatherCard(weatherData) {
+    const main = document.querySelector("main");
+    
+    // create article & append to main
+    const article = main.appendChild(createElement("article"));
+    addElementWithText(article, "h2", "Weather on Mars last week");
 
+    // error state
+    if (weatherData === undefined) {
+        addElementWithText(article, "p", "We aren't able to show weather information at the moment. Refresh the page to try again.");
+        return;
+    }
+
+    weatherData.map(function (item) {
         // create section & append to article
         const section = article.appendChild(createElement("section"));
 
         // add dayname as title to section
-        section.appendChild(createElement("h3")).appendChild(addText(`${item.day} (${item.sol} Sol)`));
+        addElementWithText(section, "h3", `${item.day} (${item.sol} Sol)`);
 
         // add list with weather data
         const ul = section.appendChild(createElement("ul"));
 
         // temperature
-        ul.appendChild(createElement("li")).appendChild(addText(item.temp.average + "\xB0C"));
+        addElementWithText(ul, "li", item.temp.average + "\xB0C");
         // wind speed
-        ul.appendChild(createElement("li")).appendChild(addText(item.wind.speed));
+        addElementWithText(ul, "li", item.wind.speed);
         // wind direction
-        ul.appendChild(createElement("li")).appendChild(addText(item.wind.direction));
+        addElementWithText(ul, "li", item.wind.direction);
     });
 }
 
@@ -28,9 +38,6 @@ function addText(text) {
     return document.createTextNode(text);
 }
 
-export const render = {
-    overview: function (weatherData, photoData) {
-        createWeatherCard(weatherData);
-    },
-    // noData: noData()
-};
+function addElementWithText(element, newElement, text) {
+    return element.appendChild(createElement(newElement)).appendChild(addText(text));
+}
