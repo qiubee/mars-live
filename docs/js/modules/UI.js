fc64e2ccd55cedbba2d9923b0291f5e96a4b8a10
qiubee <qiubees@hotmail.com>
2020-02-25 10:05:51 +0100

    update loading function

--- docs/js/modules/UI.js
+++ docs/js/modules/UI.js
@@ -1,6 +1,4 @@
 import { addElementWithText, deleteElement } from "../utils/addAndDeleteElement.js";
-export function loading(state) {
-    const article = document.querySelector("article"),
-    p = document.querySelector("article > p");
-    state ? addElementWithText(article, "p", "Loading...") : deleteElement(article, p);
+export function loading(state, element) {
+    state ? addElementWithText(element, "p", "Loading...") : deleteElement(element, "p");
 }
