aa1cfa418fc4277b61120df0f529294631182996
qiubee <qiubees@hotmail.com>
2020-02-28 13:25:11 +0100

    update api.js

--- js/modules/api.js
+++ js/modules/api.js
@@ -1,17 +1,17 @@
 import { getDate } from "../utils/date.js";
 const apiKey = "B0XkeeKZ8AD1ZEIYa7o26ya0bBDkvsXV3fn94GE2";
 
-    const mars = {
-        rover: "curiosity",
-        camera: "&camera=mast",
-        sol: "?sol=416",
-        day: `?earth_date=${getDate()}`
-    };
+const mars = {
+    rover: "curiosity",
+    camera: "&camera=mast",
+    sol: "?sol=416",
+    day: `?earth_date=${getDate()}`
+};
 
-    const endpoints = {
-        marsWeatherURL: `https://api.nasa.gov/insight_weather/?api_key=${apiKey}&feedtype=json&ver=1.0`,
-        marsPhotosURL: `https://api.nasa.gov/mars-photos/api/v1/rovers/${mars.rover}/photos${mars.sol}${mars.camera}&api_key=${apiKey}`,
-    };
+const endpoints = {
+    marsWeatherURL: `https://api.nasa.gov/insight_weather/?api_key=${apiKey}&feedtype=json&ver=1.0`,
+    marsPhotosURL: `https://api.nasa.gov/mars-photos/api/v1/rovers/${mars.rover}/photos${mars.sol}${mars.camera}&api_key=${apiKey}`,
+};
 
 async function fetchData(url) {
     const res = await fetch(url);
