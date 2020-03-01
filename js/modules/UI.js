973d2c8b02274bf6c922ab245d65c9b55d8450c3
qiubee <qiubees@hotmail.com>
2020-03-01 19:41:07 +0100

    add hideWeatherCards function

--- js/modules/UI.js
+++ js/modules/UI.js
@@ -1,4 +1,32 @@
-import { addElementWithText, deleteElement } from "../utils/element.js";
+import { addElementWithText, deleteChildElements } from "../utils/element.js";
 export function loading(state, element, loadingMessage = "Loading...") {
-    state ? addElementWithText(element, "p", loadingMessage) : deleteElement(element, "p");
+    state ? addElementWithText(element, "p", loadingMessage) : deleteChildElements(element, "p", element.lastElementChild);
+}
+
+export function hideWeatherCards(elements, day) {
+    const nodes = Array.from(elements);
+    const placeInList = nodes.map(function (node, i) {
+        if (node.hash === `#${day}`) {
+            return i;
+        }
+    }).filter(function (item) {
+        return item !== undefined;
+    })[0];
+
+    nodes.map(function (node, i) {
+        node.classList.toggle("in-active");
+        if (i !== placeInList) {
+            setTimeout(function () {
+                node.classList.toggle("hide");
+                setTimeout(function () {
+                    node.classList.toggle("hide");
+                    node.classList.toggle("hidden");
+                }, 5000);
+            }, 250 * i);
+        } else {
+            setTimeout(function () {
+                node.classList.toggle("focus");
+            }, 2500);
+        }
+    });
 }
