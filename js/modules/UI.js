6bc5f6036d6d90f27047c2481b1dc0712ec7950f
qiubee <qiubees@hotmail.com>
2020-03-01 14:29:37 +0100

    add parameter to give loading message

--- js/modules/UI.js
+++ js/modules/UI.js
@@ -1,4 +1,4 @@
 import { addElementWithText, deleteElement } from "../utils/element.js";
-export function loading(state, element) {
-    state ? addElementWithText(element, "p", "Loading...") : deleteElement(element, "p");
+export function loading(state, element, loadingMessage = "Loading...") {
+    state ? addElementWithText(element, "p", loadingMessage) : deleteElement(element, "p");
 }
